93b15e0d5c6fbd61874ef59c2f89769e212c14e1
Alexis <Yonathan.yp20@gmail.com>
2026-04-09 23:09:20 -0500

    organización en la rama de Interfaz y services

--- Registro.Application/Interfaces/IDinosaurQueryService.cs
+++ Registro.Application/Interfaces/IDinosaurQueryService.cs
@@ -6,11 +6,25 @@ namespace Registro.Application.Interfaces;
 
 public interface IDinosaurQueryService
 {
+    //Filtros
     Task<IEnumerable<DinosaurDto>> GetByZona(string zona);
     
     Task<IEnumerable<DinosaurDto>> GetBySector(string sector);
     
-    Task<IEnumerable<DinosaurDto>> GetByEdad(int edad);
+    Task<IEnumerable<DinosaurDto>> GetByAge(int age);
+    Task<IEnumerable<DinosaurDto>> GetByEspecie(string especie);
+    
+    //ordenamientos
     
     Task<IEnumerable<DinosaurDto>> GetOrdenadoPorEspecie();
+    
+    Task<IEnumerable<DinosaurDto>> OrderByZona();
+    Task<IEnumerable<DinosaurDto>> OrderBySector();
+    Task<IEnumerable<DinosaurDto>> OrderByAge();
+    Task<IEnumerable<DinosaurDto>> OrderByEspecie();
+    
+    // Agrupaciones
+    Task<IEnumerable<object>> GroupByZona();
+    Task<IEnumerable<object>> GroupBySector();
+    
 }
